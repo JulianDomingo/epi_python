e0ccf4c20a46852084127f1a9379b4f9b916614f
JulianDomingo <julian.domingo@utexas.edu>
2018-05-11 00:07:26 -0500

    h_index completed

--- epi_judge_python/h_index.py
+++ epi_judge_python/h_index.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def h_index(citations):
-    # TODO - you fill in here.
-    return 0
+    citations.sort()
+    h_ind = 0
+    c_len = len(citations)
+
+    for i, citation in enumerate(citations):
+        if citation >= (c_len - i): 
+            return (c_len - i) 
+
+    return 0 
 
 
 if __name__ == '__main__':
